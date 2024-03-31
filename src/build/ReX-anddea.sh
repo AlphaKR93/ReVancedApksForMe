5cb2db452002344e8953a0ce866519a4706b7b3c
Alpha <dev@alpha93.kr>
2024-03-31 11:07:21 +0900

    Update ReX-anddea.sh

--- src/build/ReX-anddea.sh
+++ src/build/ReX-anddea.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # ReX build
 source src/build/utils.sh
 
@@ -12,7 +13,7 @@ dl_gh "revanced-cli" "inotia00" "latest"
 
 # Patch YouTube ReX:
 get_patches_key "youtube-ReX-anddea"
-if [[ -v 1 ]]; then version=$1; else get_ver "Hide general ads" "com.google.android.youtube"; fi
+if [[ -v 2 ]]; then version=$2; else get_ver "Hide general ads" "com.google.android.youtube"; fi
 get_apk "youtube" "youtube" "google-inc/youtube/youtube"
 patch "youtube" "ReX-anddea" "inotia"
 
@@ -20,7 +21,7 @@ patch "youtube" "ReX-anddea" "inotia"
 
 # Patch YouTube Music ReX:
 get_patches_key "youtube-music-ReX-anddea"
-if [[ -v 2 ]]; then version=$2; else get_ver "Enable color match player" "com.google.android.apps.youtube.music"; fi
+if [[ -v 3 ]]; then version=$3; else get_ver "Enable color match player" "com.google.android.apps.youtube.music"; fi
 get_apk "youtube-music-arm64-v8a" "youtube-music" "google-inc/youtube-music/youtube-music" "arm64-v8a"
 patch "youtube-music-arm64-v8a" "ReX-anddea" "inotia"
 
