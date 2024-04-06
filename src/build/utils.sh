f6ebc1b9e337e6448f86631dcf3e5a229cbe172c
Alpha <dev@alpha93.kr>
2024-04-06 21:44:08 +0900

    Update utils.sh

--- src/build/utils.sh
+++ src/build/utils.sh
@@ -94,8 +94,9 @@ get_patches_key() {
 	patches=$(java -jar revanced-cli-*.jar list-patches revanced-patches-*.jar -f $1 | grep Name | cut -d " " -f 2-)
 	echo $exclude
         for patch in ${patches// /_}; do
-		if echo $exclude | grep -q $patch; then
-                        rv=${patch//_/ }
+		echo $patch
+		if (echo $exclude | grep $patch); then
+			rv=${patch//_/ }
 			echo "Included $rv"
 			includePatches+=" -i \"${rv//"\""/"\\\""}\""
 		fi
