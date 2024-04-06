6305be83050a61de59fc504d820362d9c753b738
Alpha <dev@alpha93.kr>
2024-04-06 14:16:34 +0900

    Update ReX-anddea.sh

--- src/build/ReX-anddea.sh
+++ src/build/ReX-anddea.sh
@@ -12,7 +12,7 @@ dl_gh "revanced-cli" "inotia00" "latest"
 #################################################
 
 # Patch YouTube ReX:
-get_patches_key "com.google.youtube"
+get_patches_key "com.google.android.youtube"
 if [[ -v 2 ]]; then version=$2; else get_ver "Hide general ads" "com.google.android.youtube"; fi
 get_apk "youtube" "youtube" "google-inc/youtube/youtube"
 patch "youtube" "ReX-anddea" "inotia"
