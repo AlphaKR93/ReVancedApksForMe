408cfd8578933e6a0582b041bbdb6c334f7adaf7
Alpha <dev@alpha93.kr>
2024-04-06 13:35:10 +0000

    downgradable

--- src/build/ReX-anddea-music.sh
+++ src/build/ReX-anddea-music.sh
@@ -8,6 +8,7 @@ source src/build/utils.sh
 # Download requirements
 dl_gh "revanced-patches revanced-integrations" "anddea" "latest"
 dl_gh "revanced-cli" "inotia00" "latest"
+dl_gh "lspatch" "lsposed" "latest"
 
 #################################################
 
@@ -16,5 +17,6 @@ get_patches_key "com.google.android.apps.youtube.music"
 if [[ -v 3 ]]; then version=$3; else get_ver "Enable color match player" "com.google.android.apps.youtube.music"; fi
 get_apk "youtube-music-arm64-v8a" "youtube-music" "google-inc/youtube-music/youtube-music" "arm64-v8a"
 patch "youtube-music-arm64-v8a" "ReX-anddea" "inotia"
+java -jar jar-*-release.jar ./release/*.apk --manager -o ./release -f -r
 
 #################################################
--- src/build/ReX-anddea.sh
+++ src/build/ReX-anddea.sh
@@ -8,6 +8,7 @@ source src/build/utils.sh
 # Download requirements
 dl_gh "revanced-patches revanced-integrations" "anddea" "latest"
 dl_gh "revanced-cli" "inotia00" "latest"
+dl_gh "lspatch" "lsposed" "latest"
 
 #################################################
 
@@ -16,3 +17,6 @@ get_patches_key "com.google.android.youtube"
 if [[ -v 2 ]]; then version=$2; else get_ver "Hide general ads" "com.google.android.youtube"; fi
 get_apk "youtube" "youtube" "google-inc/youtube/youtube"
 patch "youtube" "ReX-anddea" "inotia"
+java -jar jar-*-release.jar ./release/*.apk --manager -o ./release -f -r
+
+#################################################
