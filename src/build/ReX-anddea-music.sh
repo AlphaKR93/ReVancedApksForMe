3be70b3d955f79c44ea43a5a7fd3afbc0099e13c
Alpha <dev@alpha93.kr>
2024-04-11 15:37:07 +0900

    Update ReX-anddea-music.sh

--- src/build/ReX-anddea-music.sh
+++ src/build/ReX-anddea-music.sh
@@ -5,19 +5,34 @@ initialize
 
 #################################################
 
-# Download requirements
+start "Patching YouTube Music APK with ReX forked by anddea..."
+
 dl_gh "revanced-patches revanced-integrations" "anddea" "prerelease"
 dl_gh "revanced-cli" "inotia00" "latest"
-dl_gh "lspatch" "lsposed" "latest"
+dl_apk "rex-music" "youtube-music" "google-inc/youtube-music/youtube-music" "arm64-v8a"
+
+get_patches_key "com.google.android.apps.youtube.music"
+[ -z $3 ] && get_ver "Enable color match player" "com.google.android.apps.youtube.music"
+
+patch "rex-music" "anddea" "inotia"
+purge "patches.json revanced-integrations-*.apk revanced-patches-*.jar revanced-cli-*.jar"
 
 #################################################
 
-# Patch YouTube Music ReX:
-get_patches_key "com.google.android.apps.youtube.music"
-[ -z $3 ] && get_ver "Enable color match player" "com.google.android.apps.youtube.music"
-dl_apk "youtube-music-arm64-v8a" "youtube-music" "google-inc/youtube-music/youtube-music" "arm64-v8a"
-patch "youtube-music-arm64-v8a" "ReX-anddea" "inotia"
-inject "youtube-music-arm64-v8a" "ReX-anddea"
+start "Patching ReX Music APK with ReVanced..."
+
+dl_gh "ReVanced-Patches ReVanced-Integrations" "ReVanced" "prerelease"
+get_patches_key "com.google.android.apps.youtube.music" -
+patch "rex-music" "anddea" - "./release/rex-music.apk"
+purge "patches.json revanced-patches-*.jar revanced-integrations-*.apk"
+
+#################################################
+
+start "Injecting LSPatch..."
+
+dl_gh "LSPatch" "LSPosed" "latest"
+inject "rex-music" "anddea"
+purge "jar-*.jar manager-*.apk"
 
 #################################################
 
