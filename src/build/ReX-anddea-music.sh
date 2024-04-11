e06706f74584fcba993583c39ce31a5ffe6bb648
Alpha <dev@alpha93.kr>
2024-04-12 07:44:12 +0900

    Update ReX-anddea-music.sh

--- src/build/ReX-anddea-music.sh
+++ src/build/ReX-anddea-music.sh
@@ -19,15 +19,6 @@ purge "patches.json revanced-integrations-*.apk revanced-patches-*.jar revanced-
 
 #################################################
 
-start "Patching ReX Music APK with ReVanced..."
-
-dl_gh "ReVanced-Patches ReVanced-Integrations ReVanced-CLI" "ReVanced" "prerelease"
-get_patches_key "com.google.android.apps.youtube.music" -
-patch "rex-music" "anddea" - "./release/rex-music-anddea.apk"
-purge "patches.json revanced-patches-*.jar revanced-integrations-*.apk"
-
-#################################################
-
 start "Injecting LSPatch..."
 
 dl_gh "LSPatch" "LSPosed" "latest"
