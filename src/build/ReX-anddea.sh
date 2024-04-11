95d39cb11cc5f217c6b3060d22f5dbf9c914ade6
Alpha <dev@alpha93.kr>
2024-04-11 15:40:38 +0900

    Update ReX-anddea.sh

--- src/build/ReX-anddea.sh
+++ src/build/ReX-anddea.sh
@@ -5,19 +5,29 @@ initialize
 
 #################################################
 
-# Download requirements
-dl_gh "revanced-patches revanced-integrations" "anddea" "prerelease"
-dl_gh "revanced-cli" "inotia00" "latest"
-dl_gh "lspatch" "lsposed" "latest"
+start "Patching YouTube APK with ReX forked by anddea..."
+
+get_patches_key "com.google.android.youtube"
+[ -z $3 ] && get_ver "Hide general ads" "com.google.android.youtube"
+dl_apk "rex" "youtube" "google-inc/youtube/youtube"
+patch "rex" "anddea" "inotia"
 
 #################################################
 
-# Patch YouTube ReX:
-get_patches_key "com.google.android.youtube"
-[ -z $3 ] && get_ver "Hide general ads" "com.google.android.youtube"
-dl_apk "youtube" "youtube" "google-inc/youtube/youtube"
-patch "youtube" "ReX-anddea" "inotia"
-inject "youtube" "ReX-anddea"
+start "Patching RVX Extended APK with ReVanced..."
+
+dl_gh "ReVanced-Patches ReVanced-Integrations" "ReVanced" "prerelease"
+get_patches_key "com.google.android.youtube" -
+patch "rex" "anddea" - "./release/rex-anddea.apk"
+purge "patches.json revanced-patches-*.jar revanced-integrations-*.apk"
+
+#################################################
+
+start "Injecting LSPatch..."
+
+dl_gh "LSPatch" "LSPosed" "latest"
+inject "rex" "anddea"
+purge "jar-*.jar manager-*.apk"
 
 #################################################
 
