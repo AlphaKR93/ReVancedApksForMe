69436f5abf1a73d098c4642d05c9bf7cfd0d7c54
Alpha <dev@alpha93.kr>
2024-04-12 07:43:59 +0900

    Update ReX-anddea.sh

--- src/build/ReX-anddea.sh
+++ src/build/ReX-anddea.sh
@@ -19,15 +19,6 @@ purge "patches.json revanced-integrations-*.apk revanced-patches-*.jar revanced-
 
 #################################################
 
-start "Patching RVX Extended APK with ReVanced..."
-
-dl_gh "ReVanced-Patches ReVanced-Integrations ReVanced-CLI" "ReVanced" "prerelease"
-get_patches_key "com.google.android.youtube" -
-patch "rex" "anddea" - "./release/rex-anddea.apk"
-purge "patches.json revanced-patches-*.jar revanced-integrations-*.apk"
-
-#################################################
-
 start "Injecting LSPatch..."
 
 dl_gh "LSPatch" "LSPosed" "latest"
