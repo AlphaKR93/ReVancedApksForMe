68aaa5e6b279d389f615d9a17adb7678de23c35d
Alpha <dev@alpha93.kr>
2024-04-11 15:45:12 +0900

    Update ReX-anddea-music.sh

--- src/build/ReX-anddea-music.sh
+++ src/build/ReX-anddea-music.sh
@@ -9,11 +9,11 @@ start "Patching YouTube Music APK with ReX forked by anddea..."
 
 dl_gh "revanced-patches revanced-integrations" "anddea" "prerelease"
 dl_gh "revanced-cli" "inotia00" "latest"
-dl_apk "rex-music" "youtube-music" "google-inc/youtube-music/youtube-music" "arm64-v8a"
 
 get_patches_key "com.google.android.apps.youtube.music"
 [ -z $3 ] && get_ver "Enable color match player" "com.google.android.apps.youtube.music"
 
+dl_apk "rex-music" "youtube-music" "google-inc/youtube-music/youtube-music" "arm64-v8a"
 patch "rex-music" "anddea" "inotia"
 purge "patches.json revanced-integrations-*.apk revanced-patches-*.jar revanced-cli-*.jar"
 
