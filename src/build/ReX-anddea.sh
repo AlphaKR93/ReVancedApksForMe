9829de2df729e3c04930a0bb011502c1c6a9598f
Alpha <dev@alpha93.kr>
2024-07-07 02:28:56 +0000

    f-

--- src/build/ReX-anddea.sh
+++ src/build/ReX-anddea.sh
@@ -11,7 +11,7 @@ dl_gh "revanced-patches revanced-integrations" "anddea" "prerelease"
 dl_gh "revanced-cli" "inotia00" "latest"
 
 get_patches_key "com.google.android.youtube"
-[ -z "$3" ] && get_ver "Hide general ads" "com.google.android.youtube"
+[ -z "$3" ] && get_ver "Hide ads" "com.google.android.youtube"
 
 dl_apk "rex" "youtube" "google-inc/youtube/youtube"
 patch "rex" "anddea" "inotia"
