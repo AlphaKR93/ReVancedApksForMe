bf857dc9a5938253878826508b2d7b25f037fc64
Alpha <dev@alpha93.kr>
2024-08-16 11:06:08 +0900

    Update utils.sh

--- src/build/utils.sh
+++ src/build/utils.sh
@@ -215,7 +215,7 @@ _dl_apk() {
 	url="https://www.apkmirror.com$(_req "$url" - | tr '\n' ' ' | sed -n "s/href=\"/@/g; s;.*${regexp}.*;\1;p")"
 	sleep 5
 
-	url="https://www.apkmirror.com$(_req "$url" - | grep "downloadButton" | sed -n 's;.*href="\(.*key=[^"]*\)">.*;\1;p')"
+	url=$(req "$url" - | $HTMLQ --base https://www.apkmirror.com --attribute href ".downloadButton")
 	sleep 5
 
    	url=$(_req "$url" - | $htmlq --base https://www.apkmirror.com --attribute href "span > a[rel = nofollow]")
