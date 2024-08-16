3f4e6410e83a68d290d96e3d85ef132708329ce4
Alpha <dev@alpha93.kr>
2024-08-16 13:27:58 +0900

    Update ReX-anddea.sh

--- src/build/ReX-anddea.sh
+++ src/build/ReX-anddea.sh
@@ -13,7 +13,7 @@ dl_gh "revanced-cli" "inotia00" "latest"
 get_patches_key "com.google.android.youtube"
 [ -z "$3" ] && get_ver "Hide ads" "com.google.android.youtube"
 
-dl_apk "rex" "youtube" "google-inc/youtube/youtube"
+dl_apk "rex" "youtube" "google-inc/youtube/youtube" bundle
 patch "rex" "anddea" "inotia"
 purge "patches.json revanced-integrations-*.apk revanced-patches-*.jar revanced-cli-*.jar"
 
